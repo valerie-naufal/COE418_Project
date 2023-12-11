07fa10b7c19643036fbf466b8043981e29454aab
valerie-naufal <valerie.naufal@gmail.com>
2023-12-11 23:49:05 +0200

    Added reserve functionality

--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session
-import utils
+import utils, os
+from datetime import datetime
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'images')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = "27eduCBA09"
 Session(app)
 
+
 @app.route('/')
 def home():
     return render_template('main.html')
+
+@app.get('/myProfile')
+def myprofile():
+    return render_template('myProfile.html')
 
 @app.route('/explore')
 def explore():
@@ -23,7 +33,6 @@ def createPage():
 
 @app.post('/createAccount')
 def createAccount():
-    print("account created")
     firstName = request.form["firstName"]
     lastName = request.form["lastName"]
     phoneNb = request.form["phoneNb"]
@@ -35,7 +44,10 @@ def createAccount():
 
 @app.get('/listVenue')
 def listPage():
-    return render_template('listVenue.html')
+    if session["email"]:
+        return render_template('listVenue.html')
+    else:
+        redirect("/")
 
 @app.post('/listVenue')
 def listVenue():
@@ -44,7 +56,12 @@ def listVenue():
     location = request.form["location"]
     category = request.form["category"]
     capacity = request.form["capacity"]
-    utils.listVenue(name,capacity,location,category,session["email"])
+    price = request.form["price"]
+    image = request.files["imgPath"]
+    file_fullName = os.path.join(app.config["UPLOAD_FOLDER"], image.filename)
+    image.save(file_fullName)
+    imgPath = "/static/images/" + image.filename
+    utils.listVenue(name,capacity,location,category,session["email"],price,imgPath)
     return redirect("/")
 
 # POST Method to handle user sign in
@@ -54,14 +71,11 @@ def signInPost():
     # Based on user mode selector in Sign In Form
     if(data["userMode"] == True):
         status = utils.signIn(data["email"],data["password"],"OWNER")
-        print(status)
         if status == "signedIn":
-            print("inside if")
             session["user_mode"] = "OWNER"
             session["email"] = data["email"]
             return "signedIn"
         else :
-            print("inside else")
             return "error"
         
     else:
@@ -100,6 +114,23 @@ def search_route():
     data = request.get_json()
     results = utils.search(data["location"], data["category"],data["capacity"])
     return results
+
+@app.post("/reserve")
+def reserve_route():
+
+    date_time = request.form["datetime"]
+    date_parsed = datetime.strptime(date_time, '%Y-%m-%dT%H:%M')
+    date = str(date_parsed.year) + "-" + str(date_parsed.month) + "-" + str(date_parsed.day)
+    starttime = str(date_parsed.hour) + ":" + str(date_parsed.minute) 
+    photographer = None if  request.form["photographer"]=="0" else request.form["photographer"]
+    catering = None if  request.form["catering"]=="0" else request.form["catering"]
+    entertainment = None if  request.form["entertainment"]=="0" else request.form["entertainment"]
+    planner = None if  request.form["planner"]=="0" else request.form["planner"]
+    nbHours = request.form["nbHours"]
+    venue_name = request.form["venue_name"]
+    email = session["email"]
+    utils.reserve(date,photographer,catering,entertainment,planner,starttime,nbHours,venue_name,email)
+    return redirect("/")
 
 if __name__ == "__main__":
     print("Server Starting ...")
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-get_venues_query = ("SELECT name, location, category, capacity FROM venue")
+get_venues_query = ("SELECT name, location, category, capacity,price,imgPath FROM venue")
 
 get_locations_query = ("SELECT DISTINCT location FROM venue ORDER BY location")
     
@@ -9,19 +9,27 @@ get_categories_query = ("SELECT DISTINCT category FROM venue ORDER BY category")
 
 get_capacities_query = ("SELECT DISTINCT capacity FROM venue ORDER BY capacity")
 
-search_query = ("SELECT name,location,category,capacity FROM venue WHERE location=%s AND category=%s AND  capacity=%s ")
+search_query = ("SELECT name,location,category,capacity,price,imgPath FROM venue WHERE location=%s AND category=%s AND  capacity>=%s ")
 
-list_query = ("INSERT INTO venue(name,capacity,location,category,ownerId) values(%s,%s,%s,%s,%s)")
+list_query = ("INSERT INTO venue(name,capacity,location,category,ownerId,price,imgPath) values(%s,%s,%s,%s,%s,%s,%s)")
 
 owner_signIn = ("SELECT COUNT(*) AS count FROM owner WHERE email=%s AND password=%s ")
 
 ownerID_query = ("SELECT ownerId FROM owner WHERE email=%s ")
+
+custID_query = ("SELECT custId FROM customer WHERE email=%s ")
+
+venueInfo_query = ("SELECT venueID,price FROM venue WHERE name=%s ")
 
 cust_signIn = ("SELECT COUNT(*) AS count FROM customer WHERE email=%s AND password=%s ")
 
 owner_create = ("INSERT INTO owner(firstName,lastName,phoneNb,email,password) values(%s,%s,%s,%s,%s)")
 
 customer_create = ("INSERT INTO customer(firstName,lastName,phoneNb,email,password) values(%s,%s,%s,%s,%s)")
+
+insertEvent_query = ("INSERT INTO event(date,photographerID,cateringID,entertainmentID,plannerID) values(%s,%s,%s,%s,%s)")
+
+book_query = ("INSERT INTO book(eventID,venueID,custID,date,starttime,nbHours,price) value((select eventID from event where date=%s),%s,%s,%s,%s,%s,%s);")
 
 
 try:
@@ -37,8 +45,8 @@ def get_venues():
     cursor = cnx.cursor()
     cursor.execute(get_venues_query)
     
-    for(name, location, category, capacity) in cursor:
-        venues.append({"name":name, "location":location, "category":category, "capacity":capacity})
+    for(name, location, category, capacity,price,imgPath) in cursor:
+        venues.append({"name":name, "location":location, "category":category, "capacity":capacity,"price":price,"imgPath":imgPath})
 
     cursor.close()
     return venues
@@ -75,10 +83,44 @@ def search(location,category,capacity):
     cursor = cnx.cursor()
     cursor.execute(search_query,(location,category,capacity))
 
-    for(name, location, category, capacity) in cursor:
-        results.append({"name":name, "location":location, "category":category, "capacity":capacity})
+    for(name, location, category, capacity,price,imgPath) in cursor:
+        results.append({"name":name, "location":location, "category":category, "capacity":capacity,"price":price,"imgPath":imgPath})
     cursor.close()
     return results
+
+def reserve(date,photographer,catering,entertainment,planner,starttime,nbHours,venue_name,email):
+    print("in reserve")
+    cursor = cnx.cursor()
+    venueID,price = get_venueInfo(venue_name)
+    custID = get_custID(email)
+    total = float(nbHours)*float(price)
+    cursor.execute(insertEvent_query,(date,photographer,catering,entertainment,planner))
+    cnx.commit()
+    cursor.execute(book_query,(date,venueID,custID,date,starttime,nbHours,total))
+    cnx.commit()
+    cursor.close()
+
+
+def get_custID(email):
+    ID = ""
+    cursor = cnx.cursor()
+    cursor.execute(custID_query,(email,))
+    for(custId,) in cursor:
+        ID = custId
+    cursor.close()
+    return ID
+
+
+def get_venueInfo(name):
+    ID = ""
+    price = ""
+    cursor = cnx.cursor()
+    cursor.execute(venueInfo_query,(name,))
+    for(venueId,price) in cursor:
+        ID = venueId
+        price = price
+    cursor.close()
+    return ID,price
 
 def get_owner_Id(email):
     ID = ""
@@ -89,10 +131,10 @@ def get_owner_Id(email):
     cursor.close()
     return ID
 
-def listVenue(name,capacity,location,category,email):
+def listVenue(name,capacity,location,category,email,price,imgPath):
     ownerId = get_owner_Id(email)
     cursor = cnx.cursor()
-    cursor.execute(list_query,(name,capacity,location,category,ownerId))
+    cursor.execute(list_query,(name,capacity,location,category,ownerId,price,imgPath))
     cnx.commit()
     cursor.close()
 
